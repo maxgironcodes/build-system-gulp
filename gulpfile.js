0da848c862b0768e92e4b83b4965d6be3fc1530c
Max Giron <mgiron@apple.com>
2018-09-11 16:18:51 -0700

    Complete CSS build

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,59 @@
 'use strict';
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const stylelint = require('gulp-stylelint');
-const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+/* TODOS
+======================== */
+
+// Add image compression
+// Add minifiers
+// Add browser syncing
+// Add error handling
+
 
 /* Linters
 ======================== */
+
+gulp.task('lint-scss', function() {
+	return gulp.src('dev/scss/**/*.scss')
+		.pipe(stylelint({
+			syntax: 'scss',
+			configFile: '.stylelintrc.json',
+			reporters: [
+				{ formatter: 'string', console: true }
+			],
+			failAfterError: true
+		}));
+});
+
 gulp.task('lint-js', function() {
 	return gulp.src('dev/js/**/*.js')
 		.pipe(eslint())
 		.pipe(eslint.format());
 });
 
-gulp.task('lint-scss', function() {
-	return gulp.src('dev/scss/**/*.scss')
-		.pipe();
-});
-
 /* Compilers
 ======================== */
+
+gulp.task('compile-css', function() {
+	return gulp.src('dev/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('compile-js', function() {
 	return gulp.src('dev/js/**/*.js')
 		.pipe(babel())
 		.pipe(gulp.dest('dist/js'));
 });
 
-// Default
-gulp.task('default', gulp.series('lint-js', 'compile-js'));
+/* Chains
+======================== */
+
+gulp.task('linters', gulp.parallel('lint-scss', 'lint-js'));
+gulp.task('compilers', gulp.parallel('compile-css', 'compile-js'));
+gulp.task('default', gulp.series('linters', 'compilers'));
