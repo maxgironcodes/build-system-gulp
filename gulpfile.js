cc0df82d2470193413925364cc26fad58a068343
maxgironcodes <maxgironcodes@outlook.com>
2018-09-12 15:08:52 -0700

    Add image compression and code minifiers

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const stylelint = require('gulp-stylelint');
 const babel = require('gulp-babel');
+const babelmin = require('gulp-babel-minify');
 const eslint = require('gulp-eslint');
+const imagemin = require('gulp-imagemin');
 
 /* TODOS
 ======================== */
 
-// Add image compression
-// Add minifiers
 // Add browser syncing
 // Add error handling
 
@@ -35,19 +35,31 @@ gulp.task('lint-js', function() {
 		.pipe(eslint.format());
 });
 
+/* Image Compressor
+======================== */
+
+gulp.task('compress-img', function() {
+	return gulp.src('dev/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'));
+});
+
 
 /* Compilers
 ======================== */
 
 gulp.task('compile-css', function() {
 	return gulp.src('dev/scss/**/*.scss')
-		.pipe(sass())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
 		.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('compile-js', function() {
 	return gulp.src('dev/js/**/*.js')
 		.pipe(babel())
+    .pipe(babelmin())
 		.pipe(gulp.dest('dist/js'));
 });
 
@@ -57,4 +69,4 @@ gulp.task('compile-js', function() {
 
 gulp.task('linters', gulp.parallel('lint-scss', 'lint-js'));
 gulp.task('compilers', gulp.parallel('compile-css', 'compile-js'));
-gulp.task('default', gulp.series('linters', 'compilers'));
+gulp.task('default', gulp.series('linters', 'compress-img', 'compilers'));
